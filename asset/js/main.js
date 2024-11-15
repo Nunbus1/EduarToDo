70f96d39a7741419a00d72d6ca2fabd390e19867
Aubin Rochefort <aubin.rochefort@isen-ouest.yncrea.fr>
2024-11-14 21:03:03 -0500

    profile

--- asset/js/main.js
+++ asset/js/main.js
@@ -13,3 +13,17 @@ function updateDate() {
 updateDate();
 
 setInterval(updateDate, 86400000);
+
+
+document.querySelectorAll('.nav-button').forEach(button => {
+    button.addEventListener('click', () => {
+        // Cacher toutes les sections
+        document.querySelectorAll('.section').forEach(section => {
+            section.style.display = 'none';
+        });
+
+        // Afficher la section correspondant au bouton cliqué
+        const sectionId = button.getAttribute('data-section');
+        document.getElementById(sectionId).style.display = 'block';
+    });
+});
